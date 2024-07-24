8395d7e41591d8315a95200fc5249fb30e477726
hokageCodes <hashsenju94@gmail.com>
2024-07-24 01:12:53 +0100

    auth done - logic and UI

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,15 @@
+// firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDypV6lK2QtwikGSwILNHbNwNISylKT5HY",
-    authDomain: "hng-link-app.firebaseapp.com",
-    projectId: "hng-link-app",
-    storageBucket: "hng-link-app.appspot.com",
-    messagingSenderId: "334830354649",
-    appId: "1:334830354649:web:e6649984bae829946c3ec8",
-    measurementId: "G-2QNHFKD011"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 };
 
 const app = initializeApp(firebaseConfig);
